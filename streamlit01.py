80387ddc5488e2a96b2c859a5e58382c0d0d049a
globexdak <agridak@gmail.com>
2024-08-31 17:23:13 +0900

    add slit02

--- streamlit01.py
+++ streamlit01.py
@@ -6,3 +6,5 @@ st.title('hello streamlit')
 
 #to run Streamlit from the command line
 # streamlit run streamlit01.py
+# https://globexdak-streamlit-streamlit01-gicssd.streamlit.app/
+# https://streamlit.io/
